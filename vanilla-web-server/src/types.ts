373afba4059efaf25c2d1c5a49eeea2eef53a039
Joao Goncalves <goncalves.joao@gmail.com>
2026-03-20 20:50:17 +0900

    Update types.ts

--- vanilla-web-server/src/types.ts
+++ vanilla-web-server/src/types.ts
@@ -15,10 +15,6 @@ export type ServerModOutputType = {
   body?: any;
 };
 
-// export type ServerModHandlerType = (
-//   env: ServerModEnvType,
-// ) => Promise<ServerModOutputType>;
-
 export interface ServerModInterface {
   run(env: ServerModEnvType): Promise<ServerModOutputType>;
 }
