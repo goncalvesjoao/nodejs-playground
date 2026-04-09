5e649a6451a78f3e78a058a12a2e8f2829ad86c7
Joao Goncalves <goncalves.joao@gmail.com>
2026-04-09 13:30:09 +0900

    Update web-framework.test.ts

--- vanilla-web-server/test/lib/web-framework.test.ts
+++ vanilla-web-server/test/lib/web-framework.test.ts
@@ -5,12 +5,12 @@ import { RequestBuilder } from '@test/support/utils';
 
 class BaseController extends Controller {}
 
-class RootController extends BaseController {
-  @Get('')
-  async index(_req: RequestType): Promise<ResponseType> {
-    return Promise.resolve({ status: 200, body: { message: 'home page' } });
-  }
-}
+// class RootController extends BaseController {
+//   @Get('')
+//   async index(_req: RequestType): Promise<ResponseType> {
+//     return Promise.resolve({ status: 200, body: { message: 'home page' } });
+//   }
+// }
 
 class ApiController extends BaseController {
   static basePath = 'api/';
@@ -108,24 +108,24 @@ void describe('lib - web-framework - Controller', () => {
       assert.deepEqual(response.body, { message: 'posts show' });
     });
 
-  //   void test('returns a response when the request ends with "/" and action also ends with "/"', async () => {
-  //     const controller = new PostsApiController();
+    //   void test('returns a response when the request ends with "/" and action also ends with "/"', async () => {
+    //     const controller = new PostsApiController();
 
-  //     const response = await controller.handle(
-  //       RequestBuilder({ path: '/api/posts/1/edit/' }),
-  //     );
+    //     const response = await controller.handle(
+    //       RequestBuilder({ path: '/api/posts/1/edit/' }),
+    //     );
 
-  //     assert.ok(response);
-  //     assert.equal(response.status, 200);
-  //     assert.deepEqual(response.body, { message: 'posts edit' });
-  //   });
-  // });
+    //     assert.ok(response);
+    //     assert.equal(response.status, 200);
+    //     assert.deepEqual(response.body, { message: 'posts edit' });
+    //   });
+    // });
 
-  // void describe('#path', () => {
-  //   void test('returns .basePath concatenated with ancestor paths', () => {
-  //     assert.equal(new RootController().path, '/');
-  //     assert.equal(new ApiController().path, '/api');
-  //     assert.equal(new PostsApiController().path, '/api/posts');
-  //   });
+    // void describe('#path', () => {
+    //   void test('returns .basePath concatenated with ancestor paths', () => {
+    //     assert.equal(new RootController().path, '/');
+    //     assert.equal(new ApiController().path, '/api');
+    //     assert.equal(new PostsApiController().path, '/api/posts');
+    //   });
   });
 });
